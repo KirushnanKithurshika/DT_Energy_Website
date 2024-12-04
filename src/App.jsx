6571b09dc2b6e6bddc47be069c861649cbce93d4
kithurshika Kirushnan <kirushnankithurshika@gmail.com>
2024-12-04 17:09:13 +0530

    resize

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,18 @@ import Contact from './components/Contact/contact';
 import Servicepage from './pages/Servicepage/servicepage';
 import Careers from './pages/Career/career';
 import Contactus from './pages/Contactus/contactus';
+import Partners from './components/Partners/partners';
 
 function App() {
   return (
     <BrowserRouter basename="/DT_Energy_Website">
       <Routes>
-        <Route path='/' element={<Homepage />} />
+        <Route path='/' element={<Homepage/>} />
         <Route path='/product' element={<Product />} />
         <Route path='/service' element={<Servicepage />} />
         <Route path='/career' element={<Careers />} />
         <Route path='/contact' element={<Contactus />} />
+        <Route path='/partners' elment={<Partners/>}/>
       </Routes>
     </BrowserRouter>
   );
